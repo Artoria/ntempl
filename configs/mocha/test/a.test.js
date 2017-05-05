7fa3531b2b0794bfa1b01597d11d312bfad5dc55
Artoria <Artoria>
2017-05-05 18:36:44 +0800

    modified:   configs/mocha/test/a.test.js

--- configs/mocha/test/a.test.js
+++ configs/mocha/test/a.test.js
@@ -2,24 +2,24 @@ var chai = require('chai');
 var expect = chai.expect;
 var jsdom = require('jsdom').JSDOM;
 
-
-var nt = require('../../../index.js');
-describe('test', function(){
-  it('should work', function(){
-    var dom = new jsdom(`<!DOCTYPE html><div id="app">
+var dom = new jsdom(`<!DOCTYPE html><div id="app">
       <div nt-id='name'>unnamed</div>
     </div>`);
     global.document = dom.window.document, global.window = document.window;
+var nt = require('../../../index.js');
+describe('test', function(){
+  it('should work', function(){
+    
     var app = nt.app(nt.q("#app"));
     app.el('name').innerText = "Artoria";
     expect(app.get({name: 1}).name).to.eql("Artoria");
   });
 
   it('should work for set, get and modify', function(){
-    var dom = new jsdom(`<div id="app">
+    global.document.body.innerHTML = `<div id="app">
       <div nt-id='name'>unnamed</div>
       <input nt-id='age' nt-default='value'>
-    </div>`);
+    </div>`;
     global.document = dom.window.document, global.window = document.window;
     var app = require('ntempl').app(nt.q("#app"));
     app.set({name: "Hello", age: 99});
