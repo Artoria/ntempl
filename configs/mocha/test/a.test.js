4b5d04eabb8b30a40fc6a7a8d8046c2911d7a771
Artoria <Artoria>
2017-05-05 16:14:43 +0800

    modified:   configs/mocha/test/a.test.js 	modified:   package.json

--- configs/mocha/test/a.test.js
+++ configs/mocha/test/a.test.js
@@ -1,6 +1,9 @@
 var chai = require('chai');
-var ntapp = require('../../index.js');
 var expect = chai.expect;
+var jsdom = require('jsdom').JSDOM;
+var dom = new jsdom("<!DOCTYPE html><p>Hello world</p>");
+global.document = dom.window.document, global.window = document.window;
+var ntapp = require('../../../index.js');
 describe('test', function(){
   it('should work', function(){
       expect(1).to.eql(1);
