ddacc107197d9d0da2c56355d177afad1af432ab
Artoria <Artoria>
2017-05-05 16:02:06 +0800

    new file:   .gitignore 	modified:   .travis.yml 	new file:   configs/mocha/test/a.test.js 	modified:   package.json

---
+++ configs/mocha/test/a.test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var ntapp = require('../../index.js');
+var expect = chai.expect;
+describe('test', function(){
+  it('should work', function(){
+      expect(1).to.eql(1);
+  })
+})
