17f707382a6135db79f5b486d8c849eb3dafb078
Artoria <Artoria>
2017-05-05 18:18:48 +0800

    modified:   configs/mocha/test/a.test.js

--- configs/mocha/test/a.test.js
+++ configs/mocha/test/a.test.js
@@ -1,15 +1,28 @@
 var chai = require('chai');
 var expect = chai.expect;
 var jsdom = require('jsdom').JSDOM;
-var dom = new jsdom(`<!DOCTYPE html><div id="app">
-  <div nt-id='name'>unnamed</div>
-</div>`);
-global.document = dom.window.document, global.window = document.window;
+
+
 var nt = require('../../../index.js');
 describe('test', function(){
   it('should work', function(){
+    var dom = new jsdom(`<!DOCTYPE html><div id="app">
+      <div nt-id='name'>unnamed</div>
+    </div>`);
+    global.document = dom.window.document, global.window = document.window;
     var app = nt.app(nt.q("#app"));
     app.el('name').innerText = "Artoria";
     expect(app.get({name: 1}).name).to.eql("Artoria");
+  });
+
+  it('should work for set, get and modify', function(){
+    var dom = new jsdom(`<div id="app">
+      <div nt-id='name'>unnamed</div>
+      <input nt-id='age' nt-default='value'>
+    </div>`);
+    var app = require('ntempl').app(document.querySelector("#app"));
+    app.set({name: "Hello", age: 99});
+    app.modify({name: x => x.toUpperCase()});
+    expect(app.get({name: 1}).name).to.eql("HELLO");
   })
 })
